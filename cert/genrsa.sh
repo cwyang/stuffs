d8ba408fca60079b8103c7ff6aeebdea0eacc1e0
Chul-Woong Yang <cwyang@gmail.com>
2016-04-12 18:32:15 +0900

    ecdsa keygen

--- cert/genrsa.sh
+++ cert/genrsa.sh
@@ -1,9 +1,34 @@
-openssl genpkey -algorithm RSA -out ca_key.pem -pkeyopt rsa_keygen_bits:2048
-openssl req -new -key ca_key.pem -days 1096 -extensions v3_ca -batch -out ca.csr -utf8 -subj '/CN=CWYANG_TEST_CA'
-openssl x509 -req -sha256 -days 3650 -in ca.csr -signkey ca_key.pem -set_serial 1111 -extfile ca.cnf -out ca_cert.pem
+#!/bin/bash
+CARSA=$1
+SERVERRSA=$2
+KEYBIT=$3
+SCN=$4
+CCN=$5
+SDOMAIN=$6
+CDOMAIN=$7
+if [ "${CARSA}" == "1" ]; then
+    openssl genpkey -algorithm RSA -out ca_key.pem -pkeyopt rsa_keygen_bits:${KEYBIT}
+    CCNAME=${CCN}
+else
+    openssl ecparam -genkey -name secp256r1 | openssl ec -out ca_key.pem #256bit
+    CCNAME="[EC]${CCN}"
+fi
+openssl req -new -key ca_key.pem -days 1096 -extensions v3_ca -batch -out ca.csr -utf8 -subj "/CN=${CCNAME}"
+openssl x509 -req -sha256 -days 3650 -in ca.csr -signkey ca_key.pem -set_serial 1111 -out ca_cert.pem \
+    -extfile <(cat ca.cnf \
+    <(printf "subjectAltName=DNS:${CDOMAIN}"))
 
-openssl genpkey -algorithm RSA -out server_key.pem -pkeyopt rsa_keygen_bits:2048
-openssl req -new -key server_key.pem -days 1096 -extensions v3_ca -batch -out server.csr -utf8 -subj '/CN=CWYANG_TEST_SERVER'
-openssl x509 -req -sha256 -days 1096 -in ca.csr -CAkey ca_key.pem -CA ca_cert.pem -set_serial 2222 -out server_cert.pem -extfile server.cnf
+if [ "${SERVERRSA}" == "1" ]; then
+    openssl genpkey -algorithm RSA -out server_key.pem -pkeyopt rsa_keygen_bits:${KEYBIT}
+    SCNAME=${SCN}
+else
+    openssl ecparam -genkey -name secp256r1 | openssl ec -out server_key.pem  #256bit
+    SCNAME="[EC]${SCN}"
+fi
+openssl req -new -key server_key.pem -days 1096 -extensions v3_ca -batch -out server.csr -utf8 -subj "/CN=${SCNAME}"
+openssl x509 -req -sha256 -days 1096 -in ca.csr -CAkey ca_key.pem -CA ca_cert.pem -set_serial 2222 -out server_cert.pem \
+    -extfile <(cat server.cnf \
+    <(printf "subjectAltName=DNS:${SDOMAIN}
+"))
 
 cat server_key.pem server_cert.pem ca_cert.pem > server.crt
