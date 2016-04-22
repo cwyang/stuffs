3d98d4a39fdcb9778fe83aa0edac34a138afa286
Chul-Woong Yang <cwyang@gmail.com>
2016-04-22 18:36:49 +0900

    passwd specification

--- cert/genself.sh
+++ cert/genself.sh
@@ -5,13 +5,13 @@ SCN=$3
 SDOMAIN=$4
 
 if [ "${SERVERRSA}" == "1" ]; then
-    openssl genpkey -algorithm RSA -out server_key.pem -pkeyopt rsa_keygen_bits:${KEYBIT}
+    openssl genpkey -algorithm RSA -out server_key.pem -pkeyopt rsa_keygen_bits:${KEYBIT} -aes128
     SCNAME=${SCN}
 else
-    openssl ecparam -genkey -name secp256r1 | openssl ec -out server_key.pem  #256bit
+    openssl ecparam -genkey -name secp256r1 | openssl ec -out server_key.pem -aes128 #256bit
     SCNAME="[EC]${SCN}"
 fi
-openssl req -passin pass:1234 -new -key server_key.pem -days 1096 -extensions v3_ca -batch -out server.csr -utf8 -subj "/CN=${SCNAME}"
+openssl req -new -key server_key.pem -days 1096 -extensions v3_ca -batch -out server.csr -utf8 -subj "/CN=${SCNAME}"
 openssl x509 -req -sha256 -days 1096 -in server.csr -signkey server_key.pem -set_serial ${RANDOM} -out server_cert.pem \
     -extfile <(cat server.cnf \
     <(printf "subjectAltName=DNS:${SDOMAIN}
