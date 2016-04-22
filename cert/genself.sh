bf1c7e976d4cf320fa1296d0b5626d971a84c31a
Chul-Woong Yang <cwyang@gmail.com>
2016-04-22 17:50:08 +0900

    self-signed cert

---
+++ cert/genself.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+SERVERRSA=$1
+KEYBIT=$2
+SCN=$3
+SDOMAIN=$4
+
+if [ "${SERVERRSA}" == "1" ]; then
+    openssl genpkey -algorithm RSA -out server_key.pem -pkeyopt rsa_keygen_bits:${KEYBIT}
+    SCNAME=${SCN}
+else
+    openssl ecparam -genkey -name secp256r1 | openssl ec -out server_key.pem  #256bit
+    SCNAME="[EC]${SCN}"
+fi
+openssl req -passin pass:1234 -new -key server_key.pem -days 1096 -extensions v3_ca -batch -out server.csr -utf8 -subj "/CN=${SCNAME}"
+openssl x509 -req -sha256 -days 1096 -in server.csr -signkey server_key.pem -set_serial ${RANDOM} -out server_cert.pem \
+    -extfile <(cat server.cnf \
+    <(printf "subjectAltName=DNS:${SDOMAIN}
+"))
+
+cat server_key.pem server_cert.pem > server.crt
