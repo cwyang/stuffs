aec91529a52d157f2ccfef92639c0c76882f2c04
Chul-Woong Yang <cwyang@gmail.com>
2018-03-20 16:54:35 +0900

    [CA] FIX:run_revoke.sh

--- ca/run_revoke.sh
+++ ca/run_revoke.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -f
 TARGET=$1
-openssl ca -config ${TARGET}.conf -crl_reason unspecified -revoke $*
+CERTNAME=$2
+openssl ca -config ${TARGET}.conf -crl_reason unspecified -revoke ${CERTNAME}
